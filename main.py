96246c9fb622ad71865a7868ef06b44f1ebc804a
Matyas420 <98987@student.vitkovickastredni.cz>
2025-01-21 12:12:12 +0100

    change_number1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#Vytvoření pole se třemi sloupci a třemi řádky
 array = ([45, 81, 21],
-         [78, 2, 62],
+         [78, 105, 62],
          [1, 75, 32],)
 
+#Vytiskne sloupec z pole "array"
 for sloupec in array:
     print(sloupec)
+
