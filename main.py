3b12604bee0a92276787fd0f41ef3a5d3dde52c4
Matyas420 <98987@student.vitkovickastredni.cz>
2025-01-21 12:15:12 +0100

    add_row

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Vytvoření pole se třemi sloupci a třemi řádky
-array = ([45, 81, 21],
-         [78, 105, 62],
-         [1, 75, 32],)
+array = ([45, 81, 21, 7],
+         [78, 105, 62, 8],
+         [1, 75, 32, 58],
+         [4, 0, 69, 46],)
 
 #Vytiskne sloupec z pole "array"
 for sloupec in array:
